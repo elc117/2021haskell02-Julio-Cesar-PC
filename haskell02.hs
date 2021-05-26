ae2ae34679bc9c261bbc0d01b8fc61f442473034
JulioCesarPC <>
2021-05-26 20:24:22 +0000

    Fiz todos os exercicios

--- haskell02.hs
+++ haskell02.hs
@@ -1,2 +1,29 @@
 -- Prática 02 de Haskell
--- Nome: 
+-- Nome: Julio Cesar Polmann Cuencas
+
+eFebre :: Float -> Bool
+eFebre x = if x > 37.80 then True else False
+
+comFebre :: [Float] -> [Float]
+comFebre f = filter eFebre f
+
+comFebre' :: [Float] -> [Float]
+comFebre' f = filter (\f -> f > 37.80) f
+
+itemize :: [String] -> [String]
+itemize s = map (\s -> "<li>"++s++"</li>") s
+
+bigCircles :: Float -> [Float] -> [Float]
+bigCircles n f = filter ((\x y -> x < (pi*(y^2))) n) f
+
+quarentena :: [(String,Float)] -> [(String,Float)]
+quarentena tupla = filter (\(_,t) -> t > 37.8) tupla
+
+idadesEm :: [Int] -> Int -> [Int]
+idadesEm list ano = map ((\x y -> x - y)ano) list
+
+changeNames :: [String] -> [String]
+changeNames list = map (\x -> if head x == 'A' then "Super " ++ x  else x) list
+
+onlyShorts :: [String] -> [String]
+onlyShorts list = filter (\x -> length x < 5) list
